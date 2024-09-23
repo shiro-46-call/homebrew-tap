ea96bdc8f10aed2a797f663dd47b5e4fd27ea41a
shiro-46-call <yuto.4654@icloud.com>
2024-09-24 02:08:12 +0900

    アップデート

--- Formula/pygpt.rb
+++ Formula/pygpt.rb
@@ -4,7 +4,7 @@ class Pygpt < Formula
     desc "Command line tool for pygpt"
     homepage "https://github.com/shiro-46-call/pygpt"
     license "MIT"
-    url "https://github.com/shiro-46-call/pygpt/releases/download/v.1.0.1/pygpt-1.0.1.tar.gz"
+    url "https://github.com/shiro-46-call/pygpt/releases/download/v1.0.1/pygpt-1.0.1.tar.gz"
     version HOMEBREW_PYGPT_VERSION
     sha256 "fcb4ad78589883828813d61edb464298f097d70cbe7a6adb2d1b280a4b579c97" 
 
@@ -12,16 +12,18 @@ class Pygpt < Formula
         prefix.install "README.md"
         prefix.install "LICENSE"
         prefix.install "install.sh"
+        prefix.install "Example.py"
+        prefix.install "Logger.py"
 
         (prefix/"Model").install Dir["program/Model/*"]
         (prefix/"View").install Dir["program/View/*"]
-        (prefix/"bin").install "program/bin/pygpt"
+        (prefix/"bin").install "program/bin/*"
 
-        bin.install_symlink "#{prefix}/program/bin/pygpt"
+        bin.install_symlink "#{prefix}/bin/pygpt"
     end
 
     def post_install
-        system "#{prefix}/install.sh"
+        system "bash", "#{prefix}/install.sh"
     end
 
     test do
